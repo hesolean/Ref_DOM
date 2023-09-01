67c18000fc3bf03d860db0dda1b77c4422951795
unknown <lndubourg@gmail.com>
2023-09-01 12:14:12 +0200

    utilisation de React.forwardRef sur une fonction

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+const Field = React.forwardRef(function(props, ref){
+    return <div className="form-group">
+        <input type="text" className="form-control" ref={ref}/>
+    </div>
+})
+
 class Home extends React.Component {
     constructor(props) {
         super(props)
         this.handleClick = this.handleClick.bind(this)
-        this.input = null
+        //on utilise React.createRef pour récupérer la valeur de l'input
+        //associé à ref de input
+        this.input = React.createRef()
     }
 
     handleClick(e) {
-        console.log(e)
+        console.log(this.input.current.value)
     }
 
     render(){
+        console.log(this.input)
         return <div>
-            <input type="text" ref={}/>
-            <button conClick={handleClick}></button>
+            <Field ref={this.input}/>
+            {/* <input type="text" ref={this.input}/> */}
+            <button onClick={this.handleClick}>Tester</button>
         </div>
     }
 }
